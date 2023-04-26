d54cb68e424831db814f8e28d4b340c9a7ea3687
Francisco Campana <campanafrancisco1@gmail.com>
2023-04-26 16:57:42 +0200

    change config for production

--- db/db.js
+++ db/db.js
@@ -2,12 +2,12 @@ const config = require("../config/config.json");
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-   process.env.MYSQL_DATABASE || config.development.database,
-   process.env.MYSQL_USER || config.development.username,
-   process.env.MYSQL_PASSWORD || config.development.password,
+   process.env.MYSQLDATABASE || config.development.database,
+   process.env.MYSQLUSER || config.development.username,
+   process.env.MYSQLPASSWORD || config.development.password,
    {
-      host: process.env.MYSQL_HOST || config.development.host,
-      port: process.env.MYSQL_PORT || config.development.port || 3306,
+      host: process.env.MYSQLHOST || config.development.host,
+      port: process.env.MYSQLPORT || config.development.port || 3306,
       dialect: "mysql",
       pool: {
          max: 5,
