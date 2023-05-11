f6f435ae0448a4f0663ecc0360eb5db67a9ff24a
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-11 09:19:02 +0200

    change corsOptions variable for use postman

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const router = require("./router");
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 
@@ -14,15 +14,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 const corsOptions = {
-   "origin": "*",
-   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-   "preflightContinue": false,
-   "optionsSuccessStatus": 204
- }
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
@@ -32,18 +32,18 @@ app.use(router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-   next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-   // set locals, only providing error in development
-   res.locals.message = err.message;
-   res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-   // render the error page
-   res.status(err.status || 500);
-   res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
